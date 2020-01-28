1c6b7d8badeeb66def72560f12d5d04f25c9bbe8
mnakhi <mnessaakhi@gmail.com>
2020-01-28 08:27:05 -0500

    commit Amazon

--- Amazon/src/main/java/home/HomePage.java
+++ Amazon/src/main/java/home/HomePage.java
@@ -1,7 +1,94 @@
 package home;
 
 import common.CommonAPI;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class HomePage extends CommonAPI {
-    //write methods
+    @FindBy(id = "twotabsearchtextbox")
+    WebElement searchBox;
+    @FindBy(css = "input[class='nav-input']")
+    WebElement submitBtn;
+    @FindBy(css = "span[class='nav-sprite nav-logo-base']")
+    WebElement amazonLogo;
+    @FindBy(xpath = "//*[@id=\"icp-nav-flyout\"]/span/span[1]")
+    WebElement returnBtn;
+    @FindBy(id = "nav-cart")
+    WebElement shoppingCartBtn;
+    @FindBy(id = "nav-link-prime")
+    WebElement primeBtn;
+    @FindBy(id = "nav-link-accountList")
+    WebElement singInBtn;
+    @FindBy(id = "nav-hamburger-menu")
+    WebElement hamburgerIcon;
+    @FindBy(css = "a[class='nav-a nav-a-2 a-popover-trigger a-declarative']")
+    WebElement addressBtn;
+    @FindBy(xpath = "//*[@id=\"nav-xshop\"]/a[1]")
+    WebElement todaysDealBtn;
+    @FindBy(xpath = "//*[@id=\"nav-xshop\"]/a[2]")
+    WebElement bestSellersBtn;
+    @FindBy(xpath = "//*[@id=\"nav-xshop\"]/a[3]")
+    WebElement customerServiceBtn;
+    @FindBy(xpath = "//*[@id=\"nav-xshop\"]/a[4]")
+    WebElement findGiftBtn;
+    @FindBy(xpath = "//*[@id=\"nav-xshop\"]/a[5]")
+    WebElement newReleaseBtn;
+
+    public void navigateToFacebook() {
+        driver.get("https://www.amazon.com/");
+    }
+
+    public void clickOnAmazonLogo() {
+        amazonLogo.click();
+    }
+
+    public void searchNClick(String itemName) {
+        this.navigateToFacebook();
+        searchBox.sendKeys("name");
+        submitBtn.click();
+    }
+
+    public void returnNOrderProduct() {
+        returnBtn.click();
+    }
+
+    public void checkShoppingCart() {
+        shoppingCartBtn.click();
+    }
+
+    public void gotoPrime() {
+        primeBtn.click();
+    }
+
+    public void goToSignIn() {
+        singInBtn.click();
+    }
+
+    public void hamburgerIconForOptions() {
+        hamburgerIcon.click();
+    }
+
+    public void useAddressBtn() {
+        addressBtn.click();
+    }
+
+    public void useTodaysDealBtn() {
+        todaysDealBtn.click();
+    }
+
+    public void useBestSellersBtn() {
+        bestSellersBtn.click();
+    }
+
+    public void useCustomerServiceBtn() {
+        customerServiceBtn.click();
+    }
+
+    public void useFindGiftBtn() {
+        findGiftBtn.click();
+    }
+
+    public void useNewReleaseBtn() {
+        findGiftBtn.click();
+    }
 }
--- Amazon/src/test/java/home/HomeTest.java
+++ Amazon/src/test/java/home/HomeTest.java
@@ -1,7 +1,85 @@
 package home;
 
 import common.CommonAPI;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
 
 public class HomeTest extends CommonAPI {
-    //write test cases
+    @Test
+    public void testUserCanUseSearchBox() {
+        HomePage searchPage = PageFactory.initElements(driver, HomePage.class);
+        searchPage.searchNClick("phone");
+    }
+
+    @Test
+    public void testUserCanClickOnAmazonLogo() {
+        HomePage hm = PageFactory.initElements(driver, HomePage.class);
+        hm.clickOnAmazonLogo();
+    }
+
+    @Test
+    public void testUserCanClickOnReturnNOrder() {
+        HomePage hm = PageFactory.initElements(driver, HomePage.class);
+        hm.returnNOrderProduct();
+    }
+
+    @Test
+    public void testUserCanGoToShoppingCart() {
+        HomePage hm = PageFactory.initElements(driver, HomePage.class);
+        hm.checkShoppingCart();
+    }
+
+    @Test
+    public void testUserCanGoToPrime() {
+        HomePage hm = PageFactory.initElements(driver, HomePage.class);
+        hm.gotoPrime();
+    }
+
+    @Test
+    public void testUserCanGoToSignInPage() {
+        HomePage hm = PageFactory.initElements(driver, HomePage.class);
+        hm.goToSignIn();
+    }
+
+    @Test
+    public void testUserCanClickOnHamburgerIconForMoreOptions() {
+        HomePage hm = PageFactory.initElements(driver, HomePage.class);
+        hm.hamburgerIconForOptions();
+    }
+
+    @Test
+    public void testUserCanSelectAddress() {
+        HomePage hm = PageFactory.initElements(driver, HomePage.class);
+        hm.useAddressBtn();
+    }
+
+    @Test
+    public void testUserCanGoToTodaysDealPage() {
+        HomePage hm = PageFactory.initElements(driver, HomePage.class);
+        hm.useTodaysDealBtn();
+    }
+
+    @Test
+    public void testUserCanGoToBestSellersPage() {
+        HomePage hm = PageFactory.initElements(driver, HomePage.class);
+        hm.useBestSellersBtn();
+    }
+
+    @Test
+    public void testUserCanGoToCustomerServicePage() {
+        HomePage hm = PageFactory.initElements(driver, HomePage.class);
+        hm.useCustomerServiceBtn();
+    }
+
+    @Test
+    public void testUserCanGoToFindAGiftPage() {
+        HomePage hm = PageFactory.initElements(driver, HomePage.class);
+        hm.useFindGiftBtn();
+    }
+
+    @Test
+    public void testUserCanGoToNewReleasePage() {
+        HomePage hm = PageFactory.initElements(driver, HomePage.class);
+        hm.useNewReleaseBtn();
+    }
 }
