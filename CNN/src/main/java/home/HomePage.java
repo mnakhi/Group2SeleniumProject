6ad907c5a122b27ab3708b74014bc0c1fea9686b
Robert <robertavila33@gmail.com>
2020-01-28 18:37:14 -0500

    Adding CNN HomePage file

--- CNN/src/main/java/home/HomePage.java
+++ CNN/src/main/java/home/HomePage.java
@@ -1,7 +1,30 @@
 package home;
 
 import common.CommonAPI;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.testng.annotations.BeforeMethod;
+
+import java.io.IOException;
+
 
 public class HomePage extends CommonAPI {
-    //write methods
+
+    public void InvokeCNN(){
+        driver.get("https://www.cnn.com/");
+        driver.manage().window().maximize();
+    }
+   public void Clicksearch_icon(){
+        driver.findElement(By.className("search-icon")).click();
+   }
+
+   public void Exitsearch_icon(){
+        driver.findElement(By.className("close-icon")).click();
+   }
+
+
+
+
+
 }
