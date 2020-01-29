5a64b645f50a563d081d377952aca90306d0fd11
Robert <robertavila33@gmail.com>
2020-01-29 12:00:10 -0500

    Adding Hompage CNN 2nd commit

--- CNN/src/main/java/home/HomePage.java
+++ CNN/src/main/java/home/HomePage.java
@@ -16,4 +16,19 @@ public class HomePage extends CommonAPI {
     public void Exitsearch_icon(){
         driver.findElement(By.className("close-icon")).click();
     }
+
+
+    public void  clickMedia(){
+        driver.findElement(By.xpath("//ul[@class='cn cn-list-hierarchical-xs cn--idx-0 cn-container_0338BCBB-" +
+              "776B-F168-D79B-ED35F5FB6488']//a//img[@class='media__image media__image--responsive']")).click();//
+    }
+
+    public void searchAmerica(){
+        driver.findElement(By.className("search-icon")).click();
+        driver.findElement(By.xpath("//input[@id='header-search-bar']")).sendKeys("America");
+        driver.findElement(By.xpath("//div[@class='Box-sc-1fet97o-0 kJrFkT']//*[@class='search-icon']")).sendKeys();
+    }
+
+
 }
+
