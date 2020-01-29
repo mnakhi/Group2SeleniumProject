bfdc9daf2d0b9c8d4c69b12586cb91fe927fcf18
Robert <robertavila33@gmail.com>
2020-01-28 20:12:05 -0500

    added tests for cnn home page

--- CNN/src/main/java/home/HomePage.java
+++ CNN/src/main/java/home/HomePage.java
@@ -1,7 +1,19 @@
 package home;
 
 import common.CommonAPI;
+import org.openqa.selenium.By;
 
 public class HomePage extends CommonAPI {
     //write methods
+    public void InvokeCNN(){
+        driver.get("https://www.cnn.com/");
+        driver.manage().window().maximize(); // comment
+    }
+    public void Clicksearch_icon(){
+        driver.findElement(By.className("search-icon")).click();
+    }
+
+    public void Exitsearch_icon(){
+        driver.findElement(By.className("close-icon")).click();
+    }
 }
