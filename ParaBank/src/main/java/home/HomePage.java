bd965f71a467d8fbb4197acef7e18e446323c5b3
mnakhi <mnessaakhi@gmail.com>
2020-01-31 14:15:06 -0500

    after marging Facebook

--- ParaBank/src/main/java/home/HomePage.java
+++ ParaBank/src/main/java/home/HomePage.java
@@ -1,7 +1,13 @@
 package home;
 
 import common.CommonAPI;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class HomePage extends CommonAPI {
-    //write methods
+
+
+    @FindBy (xpath = "//*[@id=\"headerPanel\"]/ul[1]/li[2]/a")
+    WebElement abtUsBtn;
 }
+
