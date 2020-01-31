1db0685d59eb6a4d1fad9f54bf4731b3beb8c791
mnakhi <mnessaakhi@gmail.com>
2020-01-31 14:57:14 -0500

    MomenunAkhi's code On ParaBank(Shared Module)

--- ParaBank/src/main/java/home/HomePage.java
+++ ParaBank/src/main/java/home/HomePage.java
@@ -1,9 +1,38 @@
 package home;
 
+import common.CommonAPI;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
+public class HomePage extends CommonAPI {
+    @FindBy(xpath = "//*[@id=\"headerPanel\"]/ul[1]/li[2]/a")
+    WebElement abtUsBtn;
+    @FindBy(xpath = "//*[@id=\"headerPanel\"]/ul[1]/li[3]/a")
+    WebElement serviceBtn;
+    @FindBy(xpath="//*[@id=\"headerPanel\"]/ul[1]/li[4]/a")
+    WebElement productsBtn;
+    @FindBy(xpath = "//*[@id=\"headerPanel\"]/ul[1]/li[5]/a")
+    WebElement locationsBtn;
+    @FindBy(xpath = "//*[@id=\"headerPanel\"]/ul[1]/li[6]/a")
+    WebElement adminBtn;
+    public void navigateToAmazon() {
+        driver.get("https://parabank.parasoft.com/parabank/index.htm");
+    }
 
-public class HomePage{
-
-
+    public void clickOnAbtUs(){
+        abtUsBtn.click();
+    }
+    public void clickOnService(){
+        serviceBtn.click();
+    }
+    public void clickOnProducts(){
+        productsBtn.click();
+    }
+    public void clickOnLocations(){
+        locationsBtn.click();
+    }
+    public void clickOnAdminPage(){
+        adminBtn.click();
+    }
 }
 
--- ParaBank/src/test/java/home/HomeTest.java
+++ ParaBank/src/test/java/home/HomeTest.java
@@ -1,7 +1,34 @@
 package home;
 
 import common.CommonAPI;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
 
 public class HomeTest extends CommonAPI {
     //write test cases
+    @Test
+    public void testUserCanNavigateToAboutUsPage() throws Exception {
+        HomePage homePage= PageFactory.initElements(driver, HomePage.class);
+        homePage.clickOnAbtUs();
+    }
+    @Test
+    public void testUserCanNavigateToServicePage() throws Exception {
+        HomePage homePage= PageFactory.initElements(driver, HomePage.class);
+        homePage.clickOnService();
+    }
+    @Test
+    public void testUserCanNavigateToProductsPage() throws Exception {
+        HomePage homePage= PageFactory.initElements(driver, HomePage.class);
+        homePage.clickOnProducts();
+    }
+    @Test
+    public void testUserCanNavigateToLocationsInformationPage() throws Exception {
+        HomePage homePage= PageFactory.initElements(driver, HomePage.class);
+        homePage.clickOnLocations();
+    }
+    @Test
+    public void testUserCanNavigateToAdminPage() throws Exception {
+        HomePage homePage= PageFactory.initElements(driver, HomePage.class);
+        homePage.clickOnLocations();
+    }
 }
